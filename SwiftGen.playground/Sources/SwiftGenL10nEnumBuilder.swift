023f76e1f07a545a0c72419383e9d0261636049b
Olivier Halligon <olivier@halligon.net>
2015-07-18 18:04:18 +0200

    Cleanup SwiftGenL10nEnumBuilder to move private helpers at end + changed Entry  from typealias to struct

--- SwiftGen.playground/Sources/SwiftGenL10nEnumBuilder.swift
+++ SwiftGen.playground/Sources/SwiftGenL10nEnumBuilder.swift
@@ -2,86 +2,18 @@ import Foundation
 
 public class SwiftGenL10nEnumBuilder {
 
-    private enum PlaceholderType : String {
-        case String
-        case Float
-        case Int
-        
-        static func fromFormatChar(char: Character) -> PlaceholderType? {
-            switch char {
-            case "@":
-                return .String
-            case "f":
-                return .Float
-            case "d", "i", "u":
-                return .Int
-            default: return nil
-            }
-        }
-    }
-
-    // "I give %d apples to %@" --> [.Int, .String]
-    private func typesFromFormatString(formatString: String) -> [PlaceholderType] {
-        var types = [PlaceholderType]()
-        var placeholderIndex: Int? = nil
-        var lastPlaceholderIndex = 0
-        
-        for char in formatString.characters {
-            if char == Character("%") {
-                // TODO: Manage the "%%" special sequence
-                placeholderIndex = lastPlaceholderIndex++
-            }
-            else if placeholderIndex != nil {
-                // TODO: Manage positional placeholders like "%2$@"
-                //       That change the order the placeholder should be inserted in the types array
-                //        (If types.count+1 < placehlderIndex, we'll need to insert "Any" types to fill the gap)
-                if let type = PlaceholderType.fromFormatChar(char) {
-                    types.append(type)
-                    placeholderIndex = nil
-                }
-            }
-        }
-        
-        return types
-    }
-    
-    private typealias ParsedLine = (key: String, types: [PlaceholderType])
-    private var parsedLines = [ParsedLine]()
-
-    private func parseLine(line: String) -> ParsedLine? {
-        let range = NSRange(location: 0, length: line.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-        let regex = try! NSRegularExpression(pattern: "^\"([^\"]*)\" *= *\"(.*)\";", options: [])
-        if let match = regex.firstMatchInString(line, options: [], range: range) {
-            
-            let key = (line as NSString).substringWithRange(match.rangeAtIndex(1))
-            
-            let translation = (line as NSString).substringWithRange(match.rangeAtIndex(2))
-            let types = typesFromFormatString(translation)
-            
-            return (key, types)
-        }
-        return nil
-    }
-
-    
-    // Public interface
-    
     public init() {}
 
-    public func addLine(line: String) -> Bool {
-        if let parsed = parseLine(line) {
-            parsedLines.append(parsed)
-            return true
-        }
-        return false
-    }
-    
     public func parseLocalizableStringsFile(path: String) throws {
         let fileContent = try NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)
         let lines = fileContent.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
-        for line in lines {
-            addLine(line)
+        for case let entry? in lines.map(Entry.init) {
+            addEntry(entry)
         }
+    }
+    
+    public func addEntry(entry: Entry) {
+        parsedLines.append(entry)
     }
     
     public func build(enumName : String = "L10n") -> String {
@@ -89,11 +21,11 @@ public class SwiftGenL10nEnumBuilder {
         
         enumText += "enum \(enumName.asSwiftIdentifier()) {\n"
         
-        for (key, types) in parsedLines {
-            let caseName = key.asSwiftIdentifier(forbiddenChars: "_")
+        for entry in parsedLines {
+            let caseName = entry.key.asSwiftIdentifier(forbiddenChars: "_")
             enumText += "\tcase \(caseName)"
-            if !types.isEmpty {
-                enumText += "(" + ", ".join(types.map{ $0.rawValue }) + ")"
+            if !entry.types.isEmpty {
+                enumText += "(" + ", ".join(entry.types.map{ $0.rawValue }) + ")"
             }
             enumText += "\n"
         }
@@ -107,18 +39,18 @@ public class SwiftGenL10nEnumBuilder {
         enumText += "\tvar string : String {\n"
         enumText += "\t\tswitch self {\n"
         
-        for (key, types) in parsedLines {
-            let caseName = key.asSwiftIdentifier(forbiddenChars: "_")
+        for entry in parsedLines {
+            let caseName = entry.key.asSwiftIdentifier(forbiddenChars: "_")
             enumText += "\t\t\tcase .\(caseName)"
-            if !types.isEmpty {
-                let params = (0..<types.count).map { "let p\($0)" }
+            if !entry.types.isEmpty {
+                let params = (0..<entry.types.count).map { "let p\($0)" }
                 enumText += "(" + ", ".join(params) + ")"
             }
             enumText += ":\n"
-            enumText += "\t\t\t\treturn L10n.tr(\"\(key)\""
-            if !types.isEmpty {
+            enumText += "\t\t\t\treturn L10n.tr(\"\(entry.key)\""
+            if !entry.types.isEmpty {
                 enumText += ", "
-                let params = (0..<types.count).map { "p\($0)" }
+                let params = (0..<entry.types.count).map { "p\($0)" }
                 enumText += ", ".join(params)
             }
             enumText += ")\n"
@@ -138,6 +70,90 @@ public class SwiftGenL10nEnumBuilder {
         enumText += "}\n"
         
         return enumText
+    }
+    
+    
+    
+    // MARK: - Public Enum types
+    
+    public enum PlaceholderType : String {
+        case String
+        case Float
+        case Int
+        
+        init?(formatChar char: Character) {
+            switch char {
+            case "@":
+                self = .String
+            case "f":
+                self = .Float
+            case "d", "i", "u":
+                self = .Int
+            default:
+                return nil
+            }
+        }
+    }
+    
+    public struct Entry {
+        let key: String
+        let types: [PlaceholderType]
+        
+        init(key: String, types: [PlaceholderType]) {
+            self.key = key
+            self.types = types
+        }
+        
+        init(key: String, types: PlaceholderType...) {
+            self.key = key
+            self.types = types
+        }
+        
+        init?(line: String) {
+            let range = NSRange(location: 0, length: line.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+            let regex = try! NSRegularExpression(pattern: "^\"([^\"]*)\" *= *\"(.*)\";", options: [])
+            if let match = regex.firstMatchInString(line, options: [], range: range) {
+                
+                let key = (line as NSString).substringWithRange(match.rangeAtIndex(1))
+                
+                let translation = (line as NSString).substringWithRange(match.rangeAtIndex(2))
+                let types = SwiftGenL10nEnumBuilder.typesFromFormatString(translation)
+                
+                self = Entry(key: key, types: types)
+            }
+            return nil
+        }
+    }
+    
+    
+    
+    // MARK: - Private Helpers
+    
+    private var parsedLines = [Entry]()
+    
+    // "I give %d apples to %@" --> [.Int, .String]
+    private static func typesFromFormatString(formatString: String) -> [PlaceholderType] {
+        var types = [PlaceholderType]()
+        var placeholderIndex: Int? = nil
+        var lastPlaceholderIndex = 0
+        
+        for char in formatString.characters {
+            if char == Character("%") {
+                // TODO: Manage the "%%" special sequence
+                placeholderIndex = lastPlaceholderIndex++
+            }
+            else if placeholderIndex != nil {
+                // TODO: Manage positional placeholders like "%2$@"
+                //       That change the order the placeholder should be inserted in the types array
+                //        (If types.count+1 < placehlderIndex, we'll need to insert "Any" types to fill the gap)
+                if let type = PlaceholderType(formatChar: char) {
+                    types.append(type)
+                    placeholderIndex = nil
+                }
+            }
+        }
+        
+        return types
     }
 }
 
