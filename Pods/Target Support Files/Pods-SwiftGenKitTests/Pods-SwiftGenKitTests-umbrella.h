cdde1498a335bf5432e2c103d2e4d858edbfc46e
Olivier Halligon <olivier@halligon.net>
2015-10-13 04:56:25 +0200

    Fix Schemes and UnitTest target

--- Pods/Target Support Files/Pods-SwiftGenKitTests/Pods-SwiftGenKitTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double Pods_SwiftGenKitTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SwiftGenKitTestsVersionString[];
-
---
+++ Pods/Target Support Files/Pods-UnitTests/Pods-UnitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnitTests : NSObject
+@end
+@implementation PodsDummy_Pods_UnitTests
+@end
