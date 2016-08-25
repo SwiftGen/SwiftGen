4512f70db0831a37fcaaf1183759817c057c526d
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-08-25 18:58:04 +0200

    Add fallback level for to deep key structure

--- GenumKit/Stencil/Contexts.swift
+++ GenumKit/Stencil/Contexts.swift
@@ -161,7 +161,7 @@ extension StringsFileParser {
     }
 
     let strings = entries.map { entryToStringMapper($0, []) }
-    let structuredStrings = structure(entries, mapper: entryToStringMapper)
+    let structuredStrings = structure(entries, mapper: entryToStringMapper, currentLevel: 0, maxLevel: 5)
 
     return Context(dictionary:
       [
@@ -179,7 +179,7 @@ extension StringsFileParser {
   }
 
   typealias Mapper = (entry: Entry, keyPath: [String]) -> [String: AnyObject]
-  private func structure(entries: [Entry], keyPath: [String] = [], mapper: Mapper) -> [String: AnyObject] {
+  private func structure(entries: [Entry], keyPath: [String] = [], mapper: Mapper, currentLevel: Int, maxLevel: Int) -> [String: AnyObject] {
 
     var structuredStrings: [String: AnyObject] = [:]
 
@@ -214,11 +214,34 @@ extension StringsFileParser {
       let entriesInKeyPath = entries.filter {
         Array($0.keyStructure.map(normalize).prefix(nextLevelKeyPath.count)) == nextLevelKeyPath.map(normalize)
       }
-      subenums.append(structure(entriesInKeyPath, keyPath: nextLevelKeyPath, mapper: mapper))
+      if currentLevel >= maxLevel {
+        subenums.append(flattenedStrings(entries, keyPath: nextLevelKeyPath, mapper: mapper, level: currentLevel+1))
+      } else {
+        subenums.append(structure(entriesInKeyPath, keyPath: nextLevelKeyPath, mapper: mapper, currentLevel: currentLevel+1, maxLevel: maxLevel))
+      }
     }
 
     if !subenums.isEmpty {
       structuredStrings["subenums"] = subenums
+    }
+
+    return structuredStrings
+  }
+
+  private func flattenedStrings(entries: [Entry], keyPath: [String], mapper: Mapper, level: Int) -> [String: AnyObject] {
+
+    var structuredStrings: [String: AnyObject] = [:]
+
+    let strings = entries
+      .filter { $0.keyStructure.count >= keyPath.count+1 }
+      .map { mapper(entry: $0, keyPath: keyPath) }
+
+    if !strings.isEmpty {
+      structuredStrings["strings"] = strings
+    }
+
+    if let lastKeyPathComponent = keyPath.last {
+      structuredStrings["name"] = lastKeyPathComponent
     }
 
     return structuredStrings
