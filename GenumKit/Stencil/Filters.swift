9c65d2f2b79d5ee7f6df8234819e29741f45b939
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-12-07 18:33:02 +0100

    Replace titlecase with capitalized method
    As camel cased strings are kept using the new snake-case
    intermediate the logic of titlecase was equal to that of
    capitalizing the string. This streamlines the code.

--- GenumKit/Stencil/Filters.swift
+++ GenumKit/Stencil/Filters.swift
@@ -56,7 +56,7 @@ struct StringFilters {
 
   static func titlecase(_ value: Any?) throws -> Any? {
     guard let string = value as? String else { throw FilterError.invalidInputType }
-    return titlecase(string)
+    return string.capitalized
   }
 
   static func snakeToCamelCase(_ value: Any?) throws -> Any? {
@@ -76,21 +76,7 @@ struct StringFilters {
   static func snakeToCamelCaseNoPrefix(_ value: Any?) throws -> Any? {
     guard let string = value as? String else { throw FilterError.invalidInputType }
     let comps = snakecase(string).components(separatedBy: "_")
-    return comps.map { titlecase($0) }.joined(separator: "")
-  }
-
-  /**
-  This returns the string with its first parameter uppercased.
-  - note: This is quite similar to `capitalise` except that this filter doesn't lowercase
-          the rest of camel cased string but keeps it untouched.
-
-  - parameter string: The string to titleCase
-
-  - returns: The string with its first character uppercased, and the rest of the string is lowercased.
-  */
-  private static func titlecase(_ string: String) -> String {
-    guard let first = string.unicodeScalars.first else { return string }
-    return String(first).uppercased() + String(string.lowercased().unicodeScalars.dropFirst())
+    return comps.map { $0.capitalized }.joined(separator: "")
   }
 
   /**
