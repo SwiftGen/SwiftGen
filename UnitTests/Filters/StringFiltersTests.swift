a214133f5dc7530c50268178a528231c04dd197a
David Jennes <david.jennes@appwise.be>
2016-12-19 00:32:43 +0100

    More string filter test cases

--- UnitTests/Filters/StringFiltersTests.swift
+++ UnitTests/Filters/StringFiltersTests.swift
@@ -12,13 +12,26 @@ class StringFiltersTests: XCTestCase {
   func testLowerFirstWord() {
     let expectations = [
       "string": "string",
-      "string_with_words": "string_with_words",
-      "SomeCapString": "someCapString",
-      "": "",
+      "String": "string",
+      "strIng": "strIng",
+      "strING": "strING",
       "X": "x",
+      "x": "x",
+      "SomeCapString": "someCapString",
+      "someCapString": "someCapString",
+      "string_with_words": "string_with_words",
+      "String_with_words": "string_with_words",
+      "String_With_Words": "string_With_Words",
+      "STRing_with_words": "stRing_with_words",
+      "string_wiTH_WOrds": "string_wiTH_WOrds",
+      "": "",
       "URLChooser": "urlChooser",
+      "a__b__c": "a__b__c",
       "__y_z!": "__y_z!",
-      "PLEASESTOPSCREAMING": "pleasestopscreaming"
+      "PLEASESTOPSCREAMING": "pleasestopscreaming",
+      "PLEASESTOPSCREAMING!": "pleasestopscreaminG!",
+      "PLEASE_STOP_SCREAMING": "pleasE_STOP_SCREAMING",
+      "PLEASE_STOP_SCREAMING!": "pleasE_STOP_SCREAMING!"
     ]
 
     for (input, expected) in expectations {
@@ -27,20 +40,95 @@ class StringFiltersTests: XCTestCase {
     }
   }
 
+  func testTitlecase() {
+    let expectations = [
+      "string": "String",
+      "String": "String",
+      "strIng": "StrIng",
+      "strING": "StrING",
+      "X": "X",
+      "x": "X",
+      "SomeCapString": "SomeCapString",
+      "someCapString": "SomeCapString",
+      "string_with_words": "String_with_words",
+      "String_with_words": "String_with_words",
+      "String_With_Words": "String_With_Words",
+      "STRing_with_words": "STRing_with_words",
+      "string_wiTH_WOrds": "String_wiTH_WOrds",
+      "": "",
+      "URLChooser": "URLChooser",
+      "a__b__c": "A__b__c",
+      "__y_z!": "__y_z!",
+      "PLEASESTOPSCREAMING": "PLEASESTOPSCREAMING",
+      "PLEASESTOPSCREAMING!": "PLEASESTOPSCREAMING!",
+      "PLEASE_STOP_SCREAMING": "PLEASE_STOP_SCREAMING",
+      "PLEASE_STOP_SCREAMING!": "PLEASE_STOP_SCREAMING!"
+    ]
+
+    for (input, expected) in expectations {
+      let result = try! StringFilters.titlecase(input) as? String
+      XCTAssertEqual(result, expected)
+    }
+  }
+
   func testSnakeToCamelCase() {
     let expectations = [
       "string": "String",
-      "string_with_words": "StringWithWords",
-      "someCapString": "SomeCapString",
-      "": "",
+      "String": "String",
+      "strIng": "StrIng",
+      "strING": "StrING",
+      "X": "X",
       "x": "X",
+      "SomeCapString": "SomeCapString",
+      "someCapString": "SomeCapString",
+      "string_with_words": "StringWithWords",
+      "String_with_words": "StringWithWords",
+      "String_With_Words": "StringWithWords",
+      "STRing_with_words": "STRingWithWords",
+      "string_wiTH_WOrds": "StringWiTHWOrds",
+      "": "",
+      "URLChooser": "URLChooser",
       "a__b__c": "ABC",
-      "__y_z": "__YZ",
+      "__y_z!": "__YZ!",
+      "PLEASESTOPSCREAMING": "PLEASESTOPSCREAMING",
+      "PLEASESTOPSCREAMING!": "PLEASESTOPSCREAMING!",
+      "PLEASE_STOP_SCREAMING": "PLEASESTOPSCREAMING",
       "PLEASE_STOP_SCREAMING!": "PLEASESTOPSCREAMING!"
     ]
 
     for (input, expected) in expectations {
       let result = try! StringFilters.snakeToCamelCase(input) as? String
+      XCTAssertEqual(result, expected)
+    }
+  }
+
+  func testSnakeToCamelCaseNoPrefix() {
+    let expectations = [
+      "string": "String",
+      "String": "String",
+      "strIng": "StrIng",
+      "strING": "StrING",
+      "X": "X",
+      "x": "X",
+      "SomeCapString": "SomeCapString",
+      "someCapString": "SomeCapString",
+      "string_with_words": "StringWithWords",
+      "String_with_words": "StringWithWords",
+      "String_With_Words": "StringWithWords",
+      "STRing_with_words": "STRingWithWords",
+      "string_wiTH_WOrds": "StringWiTHWOrds",
+      "": "",
+      "URLChooser": "URLChooser",
+      "a__b__c": "ABC",
+      "__y_z!": "YZ!",
+      "PLEASESTOPSCREAMING": "PLEASESTOPSCREAMING",
+      "PLEASESTOPSCREAMING!": "PLEASESTOPSCREAMING!",
+      "PLEASE_STOP_SCREAMING": "PLEASESTOPSCREAMING",
+      "PLEASE_STOP_SCREAMING!": "PLEASESTOPSCREAMING!"
+    ]
+
+    for (input, expected) in expectations {
+      let result = try! StringFilters.snakeToCamelCaseNoPrefix(input) as? String
       XCTAssertEqual(result, expected)
     }
   }
