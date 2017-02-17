8a83133b4fc6a8a5919b4ddd1e1f8233c633a2f5
David Jennes <david.jennes@gmail.com>
2017-02-17 18:01:02 +0100

    remove some old stuff

--- scripts/swiftlint-templates.sh
+++
@@ -1,9 +0,0 @@
-if which swiftlint >/dev/null; then
-  # Lint generated code in UnitTests/expected
-  for f in `find "${PROJECT_DIR}/Resources/Expected" -name '*.swift.out'`
-  do
-    cat $f | swiftlint lint --use-stdin | sed s:'<nopath>':"$f":
-  done
-else
-  echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
