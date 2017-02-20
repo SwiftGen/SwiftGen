a8fa522b43cf64a3d84fa06deea5553fc57b65eb
David Jennes <david.jennes@gmail.com>
2017-02-20 20:06:12 +0100

    newer swiftlint

--- scripts/install_swiftlint.sh
+++ scripts/install_swiftlint.sh
@@ -3,11 +3,13 @@
 # Installs the SwiftLint package.
 # Tries to get the precompiled .pkg file from Github, but if that
 # fails just recompiles from source.
+#
+# Original script: https://alexplescan.com/posts/2016/03/03/setting-up-swiftlint-on-travis-ci/
 
 set -e
 
 SWIFTLINT_PKG_PATH="/tmp/SwiftLint.pkg"
-SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.9.1/SwiftLint.pkg"
+SWIFTLINT_PKG_URL="https://github.com/realm/SwiftLint/releases/download/0.16.1/SwiftLint.pkg"
 
 curl -Lo $SWIFTLINT_PKG_PATH $SWIFTLINT_PKG_URL
 
