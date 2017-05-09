ae7f263d967585debdc01fe2de524400bfa4295e
David Jennes <david.jennes@gmail.com>
2017-05-09 21:26:48 +0200

    update templates command to match changes

--- Sources/templates.swift
+++ Sources/templates.swift
@@ -19,32 +19,33 @@ let templatesListCommand = command(
   },
   outputOption
 ) { onlySubcommand, output in
-  let customTemplates = (try? appSupportTemplatesPath.children()) ?? []
-  let bundledTemplates = (try? bundledTemplatesPath.children()) ?? []
   var outputLines = [String]()
 
-  let printTemplates = { (prefix: String, list: [Path]) in
-    for file in list where file.lastComponent.hasPrefix("\(prefix)-") && file.extension == "stencil" {
+  let printTemplates = { (subcommand: String, path: Path) in
+    guard let files = try? (path + subcommand).children() else { return }
+
+    for file in files where file.extension == "stencil" {
       let basename = file.lastComponentWithoutExtension
-      let idx = basename.index(basename.startIndex, offsetBy: prefix.characters.count+1)
-      let name = basename[idx..<basename.endIndex]
-      outputLines.append("   - \(name)")
+      outputLines.append("   - \(basename)")
     }
   }
 
   let subcommandsToList = onlySubcommand.isEmpty ? allSubcommands : [onlySubcommand]
-  for prefix in subcommandsToList {
-    outputLines.append("\(prefix):")
+  for subcommand in subcommandsToList {
+    outputLines.append("\(subcommand):")
     outputLines.append("  custom:")
-    printTemplates(prefix, customTemplates)
+    printTemplates(subcommand, appSupportTemplatesPath)
     outputLines.append("  bundled:")
-    printTemplates(prefix, bundledTemplates)
+    printTemplates(subcommand, bundledTemplatesPath)
   }
 
   outputLines.append("---")
   outputLines.append("You can add custom templates in \(appSupportTemplatesPath).")
-  outputLines.append("Simply name them 'subcmd-customname.stencil' where subcmd is one of the swiftgen subcommand,")
-  outputLines.append("namely " + allSubcommands.map({"\($0)-xxx.stencil"}).joined(separator: ", ") + ".")
+  outputLines.append("Simply place them in a subfolder of this path with as name one of the swiftgen subcommands,")
+  outputLines.append("namely " + allSubcommands.map({"'\($0)'"}).joined(separator: ", ") + ". Your template must be")
+  outputLines.append("in the right subfolder, and must have as extension '.stencil'. For example, if you created a")
+  outputLines.append("custom template for the strings command, place it in the following path:")
+  outputLines.append("strings/customname.stencil")
   outputLines.append("")
 
   output.write(content: outputLines.joined(separator: "\n"))
@@ -52,21 +53,14 @@ let templatesListCommand = command(
 
 private func templatePathCommandGenerator(execute: @escaping (Path, OutputDestination) throws -> Void) -> CommandType {
   return command(
-    Argument<String>("name",
-      description: "the name of the template to find, like `colors` for the default one" +
-      " or `colors-rawValue' for a specific one"),
+    Argument<String>("subcommand",
+      description: "the name of the subcommand for the template, like `colors`"),
+    Argument<String>("template",
+      description: "the name of the template to find, like `swift3` or `dot-syntax`"),
     outputOption
-  ) { name, output in
+  ) { subcommand, name, output in
     do {
-      let (prefix, shortName): (String, String)
-      if let hyphenPos = name.characters.index(of: "-") {
-        prefix = String(name.characters[name.characters.startIndex..<hyphenPos])
-        shortName = String(name.characters[name.characters.index(after: hyphenPos)..<name.characters.endIndex])
-      } else {
-        prefix = name
-        shortName = "default"
-      }
-      let path = try findTemplate(subcommand: prefix, templateShortName: shortName, templateFullPath: "")
+      let path = try findTemplate(subcommand: subcommand, templateShortName: name, templateFullPath: "")
       try execute(path, output)
     } catch {
       printError(string: "error: failed to read template: \(error)")
