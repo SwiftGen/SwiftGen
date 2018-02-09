f1bb302e32a507bdc1155f2291bbbea580e6b635
Ian Keen <iankeen82@gmail.com>
2018-02-08 17:21:05 -0800

    Add accessibility test fixtures

---
+++ Tests/Fixtures/Generated/Identifiers-iOS/properties-context-all.swift
@@ -0,0 +1,68 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+// swiftlint:disable identifier_name
+
+internal enum AccessibilityIdentifier {
+  internal enum ChildViewController: String {
+    internal static let ChildViewController_button = "ChildViewController-button"
+    internal static let ChildViewController_textfield = "ChildViewController-textfield"
+  }
+  internal enum CollectionViewCell: String {
+    internal static let CollectionViewCell_button = "CollectionViewCell-button"
+    internal static let CollectionViewCell_textfield = "CollectionViewCell-textfield"
+  }
+  internal enum ContainerViewController: String {
+    internal static let ContainerViewController_button = "ContainerViewController-button"
+    internal static let ContainerViewController_textfield = "ContainerViewController-textfield"
+  }
+  internal enum DetailViewController: String {
+    internal static let DetailViewController_button = "DetailViewController-button"
+    internal static let DetailViewController_textfield = "DetailViewController-textfield"
+  }
+  internal enum MasterViewTableViewCell: String {
+    internal static let MasterViewTableViewCell_button = "MasterViewTableViewCell-button"
+    internal static let MasterViewTableViewCell_textfield = "MasterViewTableViewCell-textfield"
+  }
+  internal enum TabBarPageViewController: String {
+    internal static let TabBarPageViewController_button = "TabBarPageViewController-button"
+    internal static let TabBarPageViewController_textfield = "TabBarPageViewController-textfield"
+  }
+  internal enum TableViewCell: String {
+    internal static let TableViewCell_button = "TableViewCell-button"
+    internal static let TableViewCell_textfield = "TableViewCell-textfield"
+  }
+  internal enum TableViewController: String {
+    internal static let TableViewController_button = "TableViewController-button"
+  }
+  internal enum ViewController: String {
+    internal static let ViewController_button = "ViewController-button"
+    internal static let ViewController_textfield = "ViewController-textfield"
+  }
+  internal enum XibCollectionViewCell: String {
+    internal static let XibCollectionViewCell_button = "XibCollectionViewCell-button"
+    internal static let XibCollectionViewCell_textfield = "XibCollectionViewCell-textfield"
+  }
+  internal enum XibDetailViewController: String {
+    internal static let XibDetailViewController_button = "XibDetailViewController-button"
+    internal static let XibDetailViewController_textfield = "XibDetailViewController-textfield"
+  }
+  internal enum XibTabBarPageViewController: String {
+    internal static let XibTabBarPageViewController_button = "XibTabBarPageViewController-button"
+    internal static let XibTabBarPageViewController_textfield = "XibTabBarPageViewController-textfield"
+  }
+  internal enum XibTableViewCell: String {
+    internal static let XibTableViewCell_button = "XibTableViewCell-button"
+    internal static let XibTableViewCell_textfield = "XibTableViewCell-textfield"
+  }
+  internal enum XibTableViewController: String {
+    internal static let XibTableViewController_button = "XibTableViewController-button"
+  }
+  internal enum XibViewController: String {
+    internal static let XibViewController_button = "XibViewController-button"
+    internal static let XibViewController_textfield = "XibViewController-textfield"
+  }
+}
+
+// swiftlint:enable identifier_name line_length type_body_length
