c0020e1a9fb2e342cbc2680c69bad60056638313
David Jennes <david.jennes@gmail.com>
2018-04-30 22:26:39 +0200

    SwiftGen: rename command and add an alias

--- Sources/SwiftGen/ParserCLI.swift
+++ Sources/SwiftGen/ParserCLI.swift
@@ -12,8 +12,21 @@ import SwiftGenKit
 public struct ParserCLI {
   public let parserType: Parser.Type
   public let name: String
+  public let templateFolder: String
   public let description: String
   public let pathDescription: String
+}
+
+extension ParserCLI {
+  init(parserType: Parser.Type, name: String, description: String, pathDescription: String) {
+    self.init(
+      parserType: parserType,
+      name: name,
+      templateFolder: name,
+      description: description,
+      pathDescription: pathDescription
+    )
+  }
 }
 
 // MARK: - All Parser Commands
@@ -32,21 +45,30 @@ let allParserCommands: [ParserCLI] = [
     pathDescription: "Colors.txt|.clr|.xml|.json file(s) to parse."
   ),
   .init(
+    parserType: Fonts.Parser.self,
+    name: "fonts",
+    description: "generate code for your fonts",
+    pathDescription: "Directory(ies) to parse."
+  ),
+  .init(
+    parserType: InterfaceBuilder.Parser.self,
+    name: "ib",
+    description: "generate code for your storyboard scenes and segues",
+    pathDescription: "Directory to scan for .storyboard files. Can also be a path to a single .storyboard"
+  ),
+  .init(
     parserType: Strings.Parser.self,
     name: "strings",
     description: "generate code for your Localizable.strings file(s)",
     pathDescription: "Strings file(s) to parse."
   ),
+
+  // Deprecated
   .init(
-    parserType: Storyboards.Parser.self,
+    parserType: InterfaceBuilder.Parser.self,
     name: "storyboards",
-    description: "generate code for your storyboard scenes and segues",
+    templateFolder: "ib",
+    description: "DEPRECATED, please use 'ib' instead",
     pathDescription: "Directory to scan for .storyboard files. Can also be a path to a single .storyboard"
-  ),
-  .init(
-    parserType: Fonts.Parser.self,
-    name: "fonts",
-    description: "generate code for your fonts",
-    pathDescription: "Directory(ies) to parse."
   )
 ]
