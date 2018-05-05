98edd329bb9a0e91f8b39a4722af5837112322c1
David Jennes <david.jennes@gmail.com>
2018-05-05 06:25:29 +0200

    JSON template (and output)

---
+++ Tests/Fixtures/Generated/JSON/swift3-context-all-publicAccess.swift
@@ -0,0 +1,63 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+private func objectFromJSON<T>(at path: String) -> T {
+  let bundle = Bundle(for: BundleToken.self)
+  guard let url = bundle.url(forResource: path, withExtension: nil),
+    let json = try? JSONSerialization.jsonObject(with: Data(contentsOf: url), options: []),
+    let result = json as? T else {
+    fatalError("Unable to load JSON at path: \(path)")
+  }
+  return result
+}
+
+private struct JSONDocument {
+  let data: [String: Any]
+
+  init(path: String) {
+    self.data = objectFromJSON(at: path)
+  }
+
+  subscript<T>(key: String) -> T {
+    guard let result = data[key] as? T else {
+      fatalError("Property '\(key)' is not of type \(T.self)")
+    }
+    return result
+  }
+}
+
+// swiftlint:disable identifier_name line_length type_body_length
+public enum JSON {
+  public enum Documents {
+    public enum Document1 {
+      public static let items: [String] = objectFromJSON(at: "documents.yaml")
+    }
+    public enum Document2 {
+      public static let items: [String] = objectFromJSON(at: "documents.yaml")
+    }
+  }
+  public enum Json {
+    private static let _document = JSONDocument(path: "json.json")
+    public static let key1: String = _document["key1"]
+    public static let key2: String = _document["key2"]
+    public static let key3: [String: Any] = _document["key3"]
+  }
+  public enum Mapping {
+    private static let _document = JSONDocument(path: "mapping.yaml")
+    public static let key1: String = _document["key1"]
+    public static let key2: [String: Any] = _document["key2"]
+  }
+  public enum Scalar {
+    public static let value: String = objectFromJSON(at: "scalar.yaml")
+  }
+  public enum Sequence {
+    public static let items: [String] = objectFromJSON(at: "sequence.yaml")
+  }
+}
+// swiftlint:enable identifier_name line_length type_body_length
+
+private final class BundleToken {}
---
+++ Tests/Fixtures/Generated/JSON/swift4-context-all.swift
@@ -0,0 +1,63 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+private func objectFromJSON<T>(at path: String) -> T {
+  let bundle = Bundle(for: BundleToken.self)
+  guard let url = bundle.url(forResource: path, withExtension: nil),
+    let json = try? JSONSerialization.jsonObject(with: Data(contentsOf: url), options: []),
+    let result = json as? T else {
+    fatalError("Unable to load JSON at path: \(path)")
+  }
+  return result
+}
+
+private struct JSONDocument {
+  let data: [String: Any]
+
+  init(path: String) {
+    self.data = objectFromJSON(at: path)
+  }
+
+  subscript<T>(key: String) -> T {
+    guard let result = data[key] as? T else {
+      fatalError("Property '\(key)' is not of type \(T.self)")
+    }
+    return result
+  }
+}
+
+// swiftlint:disable identifier_name line_length type_body_length
+internal enum JSON {
+  internal enum Documents {
+    internal enum Document1 {
+      internal static let items: [String] = objectFromJSON(at: "documents.yaml")
+    }
+    internal enum Document2 {
+      internal static let items: [String] = objectFromJSON(at: "documents.yaml")
+    }
+  }
+  internal enum Json {
+    private static let _document = JSONDocument(path: "json.json")
+    internal static let key1: String = _document["key1"]
+    internal static let key2: String = _document["key2"]
+    internal static let key3: [String: Any] = _document["key3"]
+  }
+  internal enum Mapping {
+    private static let _document = JSONDocument(path: "mapping.yaml")
+    internal static let key1: String = _document["key1"]
+    internal static let key2: [String: Any] = _document["key2"]
+  }
+  internal enum Scalar {
+    internal static let value: String = objectFromJSON(at: "scalar.yaml")
+  }
+  internal enum Sequence {
+    internal static let items: [String] = objectFromJSON(at: "sequence.yaml")
+  }
+}
+// swiftlint:enable identifier_name line_length type_body_length
+
+private final class BundleToken {}
