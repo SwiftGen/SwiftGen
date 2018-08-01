4d546a60e5c4969836f05139793c3876f56abee1
Grant Butler <grantjbutler@users.noreply.github.com>
2018-08-01 11:58:03 -0400

    Switch FetchRequest to a struct.

--- Sources/SwiftGenKit/Parsers/CoreData/FetchRequest.swift
+++ Sources/SwiftGenKit/Parsers/CoreData/FetchRequest.swift
@@ -10,8 +10,9 @@ import Foundation
 import Kanna
 
 extension CoreData {
-  public final class FetchRequest {
+  public struct FetchRequest {
     public let name: String
+    public let entity: String
 
     public let fetchLimit: Int
     public let fetchBatchSize: Int
@@ -24,35 +25,6 @@ extension CoreData {
     public let includesPendingChanges: Bool
     public let returnsObjectsAsFaults: Bool
     public let returnDistinctResults: Bool
-
-    public let entity: String
-
-    init(
-      name: String,
-      entity: String,
-      fetchLimit: Int,
-      fetchBatchSize: Int,
-      predicateString: String,
-      resultType: ResultType,
-      includeSubentities: Bool,
-      includePropertyValues: Bool,
-      includesPendingChanges: Bool,
-      returnsObjectsAsFaults: Bool,
-      returnDistinctResults: Bool
-    ) {
-      self.name = name
-      self.entity = entity
-      self.fetchLimit = fetchLimit
-      self.fetchBatchSize = fetchBatchSize
-      self.predicateString = predicateString
-      self.resultType = resultType
-
-      self.includeSubentities = includeSubentities
-      self.includePropertyValues = includePropertyValues
-      self.includesPendingChanges = includesPendingChanges
-      self.returnsObjectsAsFaults = returnsObjectsAsFaults
-      self.returnDistinctResults = returnDistinctResults
-    }
 
     public enum ResultType: String {
       case object     = "0"
@@ -79,7 +51,7 @@ private enum XML {
 }
 
 extension CoreData.FetchRequest {
-  convenience init(with object: Kanna.XMLElement) throws {
+  init(with object: Kanna.XMLElement) throws {
     guard let name = object[XML.Attributes.name] else {
       throw CoreData.ParserError.invalidFormat(reason: "Missing required fetch request name.")
     }
