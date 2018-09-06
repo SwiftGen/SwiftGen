99226f33f4f21f2e791b17f20041f69b768b918d
David Jennes <david.jennes@gmail.com>
2018-09-06 02:02:33 +0200

    Update compilation environment
    t

--- Tests/Fixtures/CompilationEnvironment/Definitions.swift
+++ Tests/Fixtures/CompilationEnvironment/Definitions.swift
@@ -1,13 +1,39 @@
 #if os(iOS)
-  import UIKit
 
-  class CreateAccViewController: UIViewController {
-  }
-  class XXPickerViewController: UIViewController {
-  }
+import UIKit
+
+final class CreateAccViewController: UIViewController {}
+final class PickerViewController: UIViewController {}
+final class SlideLeftSegue: UIStoryboardSegue {}
+final class SlideUpSegue: UIStoryboardSegue {}
+
+#if DEFINE_EXTRA_MODULE_TYPES
+final class SlideDownSegue: UIStoryboardSegue {}
+final class ValidatePasswordViewController: UIViewController {}
+#elseif DEFINE_NAMESPACED_EXTRA_MODULE_TYPES
+enum ExtraModule {
+  final class SlideDownSegue: UIStoryboardSegue {}
+  final class ValidatePasswordViewController: UIViewController {}
+}
+#endif
+
 #elseif os(OSX)
-  import Cocoa
 
-  class CustomTabViewController: NSWindowController {
-  }
+import Cocoa
+
+final class CustomTabViewController: NSWindowController {}
+final class DetailsViewController: NSWindowController {}
+final class RotateSegue: NSStoryboardSegue {}
+final class ZoomSegue: NSStoryboardSegue {}
+
+#if DEFINE_EXTRA_MODULE_TYPES
+final class LoginSegue: NSStoryboardSegue {}
+final class LoginViewController: NSWindowController {}
+#elseif DEFINE_NAMESPACED_EXTRA_MODULE_TYPES
+enum ExtraModule {
+  final class LoginSegue: NSStoryboardSegue {}
+  final class LoginViewController: NSWindowController {}
+}
+#endif
+
 #endif
--- Tests/Fixtures/CompilationEnvironment/Modules/CustomSegue.swift
+++
@@ -1,4 +0,0 @@
-import UIKit
-
-public class CustomSegue: UIStoryboardSegue {
-}
