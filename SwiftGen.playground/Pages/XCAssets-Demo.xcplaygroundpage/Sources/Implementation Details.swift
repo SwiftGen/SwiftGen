dcfd2b4cde776a84b4b53d857afa24ed92979cc6
David Jennes <david.jennes@gmail.com>
2018-09-11 23:36:17 +0200

    Update existing pages with latest generated code, and split off implementation details into a hidden file

---
+++ SwiftGen.playground/Pages/XCAssets-Demo.xcplaygroundpage/Sources/Implementation Details.swift
@@ -0,0 +1,105 @@
+#if os(OSX)
+  import AppKit.NSImage
+  public typealias AssetColorTypeAlias = NSColor
+  public typealias AssetImageTypeAlias = NSImage
+#elseif os(iOS) || os(tvOS) || os(watchOS)
+  import UIKit.UIImage
+  public typealias AssetColorTypeAlias = UIColor
+  public typealias AssetImageTypeAlias = UIImage
+#endif
+
+public var bundle: Bundle!
+
+// MARK: - Implementation Details
+
+public struct ColorAsset {
+  public fileprivate(set) var name: String
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, OSX 10.13, *)
+  public var color: AssetColorTypeAlias {
+    return AssetColorTypeAlias(asset: self)
+  }
+
+  // Extra for playgrounds
+  public init(name: String) {
+    self.name = name
+  }
+}
+
+public extension AssetColorTypeAlias {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, OSX 10.13, *)
+  convenience init!(asset: ColorAsset) {
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(OSX)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+public struct DataAsset {
+  public fileprivate(set) var name: String
+
+  #if os(iOS) || os(tvOS) || os(OSX)
+  @available(iOS 9.0, tvOS 9.0, OSX 10.11, *)
+  public var data: NSDataAsset {
+    return NSDataAsset(asset: self)
+  }
+  #endif
+
+  // Extra for playgrounds
+  public init(name: String) {
+    self.name = name
+  }
+}
+
+#if os(iOS) || os(tvOS) || os(OSX)
+@available(iOS 9.0, tvOS 9.0, OSX 10.11, *)
+public extension NSDataAsset {
+  convenience init!(asset: DataAsset) {
+    #if os(iOS) || os(tvOS)
+    self.init(name: asset.name, bundle: bundle)
+    #elseif os(OSX)
+    self.init(name: NSDataAsset.Name(asset.name), bundle: bundle)
+    #endif
+  }
+}
+#endif
+
+public struct ImageAsset {
+  public fileprivate(set) var name: String
+
+  public var image: AssetImageTypeAlias {
+    #if os(iOS) || os(tvOS)
+    let image = AssetImageTypeAlias(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(OSX)
+    let image = bundle.image(forResource: NSImage.Name(name))
+    #elseif os(watchOS)
+    let image = AssetImageTypeAlias(named: name)
+    #endif
+    guard let result = image else { fatalError("Unable to load image named \(name).") }
+    return result
+  }
+
+  // Extra for playgrounds
+  public init(name: String) {
+    self.name = name
+  }
+}
+
+public extension AssetImageTypeAlias {
+  @available(iOS 1.0, tvOS 1.0, watchOS 1.0, *)
+  @available(OSX, deprecated,
+    message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
+  convenience init!(asset: ImageAsset) {
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(OSX)
+    self.init(named: NSImage.Name(asset.name))
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
