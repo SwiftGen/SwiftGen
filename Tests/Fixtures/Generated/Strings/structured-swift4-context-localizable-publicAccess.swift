6e2df74a20c873b11477711262c81ee3f9c8883d
David Jennes <david.jennes@gmail.com>
2019-01-28 01:04:43 +0100

    Fix NSLocalizedString warning

--- Tests/Fixtures/Generated/Strings/structured-swift4-context-localizable-publicAccess.swift
+++ Tests/Fixtures/Generated/Strings/structured-swift4-context-localizable-publicAccess.swift
@@ -90,6 +90,7 @@ public enum L10n {
 
 extension L10n {
   private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    // swiftlint:disable:next nslocalizedstring_key
     let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
     return String(format: format, locale: Locale.current, arguments: args)
   }
