8207a684ae04d632ab9ec53325d51e0e7bc077cc
David Jennes <david.jennes@gmail.com>
2019-02-05 23:49:02 +0100

    Generate output

--- Tests/Fixtures/Generated/CoreData/swift4-context-empty.swift
+++ Tests/Fixtures/Generated/CoreData/swift4-context-empty.swift
@@ -1,10 +1,11 @@
 // swiftlint:disable all
 // Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable sorted_imports
 import CoreData
 import Foundation
 
-// swiftlint:disable superfluous_disable_command
 // swiftlint:disable attributes file_length vertical_whitespace_closing_braces
 // swiftlint:disable identifier_name line_length type_body_length
 
