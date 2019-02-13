6b5b573fe59dc189dadc11fe845a40dae7768a39
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-02-13 15:06:20 +0100

    Attempt to add tests for new simple-swift4 xcassets template

---
+++ Tests/Fixtures/Generated/XCAssets/simple-swift4/all-allValues.swift
@@ -0,0 +1,84 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+#if os(OSX)
+  import AppKit.NSImage
+#elseif os(iOS) || os(tvOS) || os(watchOS)
+  import UIKit.UIImage
+#endif
+
+
+// MARK: - Asset Catalogs
+
+internal enum Asset {
+  internal enum Colors {
+    internal enum _24Vision {
+      internal static let background = ColorAsset(name: "24Vision/Background")
+      internal static let primary = ColorAsset(name: "24Vision/Primary")
+    }
+    internal static let orange = ImageAsset(name: "Orange")
+    internal enum Vengo {
+      internal static let primary = ColorAsset(name: "Vengo/Primary")
+      internal static let tint = ColorAsset(name: "Vengo/Tint")
+    }
+    internal static let allColors: [ColorAsset] = [
+      _24Vision.background,
+      _24Vision.primary,
+      Vengo.primary,
+      Vengo.tint,
+    ]
+    internal static let allDataAssets: [DataAsset] = [
+    ]
+    internal static let allImages: [ImageAsset] = [
+      orange,
+    ]
+  }
+  internal enum Data {
+    internal static let data = DataAsset(name: "Data")
+    internal enum Json {
+      internal static let data = DataAsset(name: "Json/Data")
+    }
+    internal static let readme = DataAsset(name: "README")
+    internal static let allColors: [ColorAsset] = [
+    ]
+    internal static let allDataAssets: [DataAsset] = [
+      data,
+      Json.data,
+      readme,
+    ]
+    internal static let allImages: [ImageAsset] = [
+    ]
+  }
+  internal enum Images {
+    internal enum Exotic {
+      internal static let banana = ImageAsset(name: "Exotic/Banana")
+      internal static let mango = ImageAsset(name: "Exotic/Mango")
+    }
+    internal enum Round {
+      internal static let apricot = ImageAsset(name: "Round/Apricot")
+      internal static let apple = ImageAsset(name: "Round/Apple")
+      internal enum Double {
+        internal static let cherry = ImageAsset(name: "Round/Double/Cherry")
+      }
+      internal static let tomato = ImageAsset(name: "Round/Tomato")
+    }
+    internal static let `private` = ImageAsset(name: "private")
+    internal static let allColors: [ColorAsset] = [
+    ]
+    internal static let allDataAssets: [DataAsset] = [
+    ]
+    internal static let allImages: [ImageAsset] = [
+      Exotic.banana,
+      Exotic.mango,
+      Round.apricot,
+      Round.apple,
+      Round.Double.cherry,
+      Round.tomato,
+      `private`,
+    ]
+  }
+}
+
+// MARK: - Implementation Details
+
+private final class BundleToken {}
