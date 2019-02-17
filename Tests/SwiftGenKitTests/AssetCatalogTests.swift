618e4b31cb0ecbb342b723e01d50a609a0b7a4e9
David Jennes <david.jennes@gmail.com>
2019-02-17 22:34:48 +0100

    Add a `ParserOptionValues` type

--- Tests/SwiftGenKitTests/AssetCatalogTests.swift
+++ Tests/SwiftGenKitTests/AssetCatalogTests.swift
@@ -9,15 +9,15 @@ import SwiftGenKit
 import XCTest
 
 class AssetCatalogTests: XCTestCase {
-  func testEmpty() {
-    let parser = AssetsCatalog.Parser()
+  func testEmpty() throws {
+    let parser = try AssetsCatalog.Parser()
 
     let result = parser.stencilContext()
     XCTDiffContexts(result, expected: "empty", sub: .xcassets)
   }
 
   func testColors() throws {
-    let parser = AssetsCatalog.Parser()
+    let parser = try AssetsCatalog.Parser()
     try parser.searchAndParse(path: Fixtures.path(for: "Styles.xcassets", sub: .xcassets))
 
     let result = parser.stencilContext()
@@ -25,7 +25,7 @@ class AssetCatalogTests: XCTestCase {
   }
 
   func testData() throws {
-    let parser = AssetsCatalog.Parser()
+    let parser = try AssetsCatalog.Parser()
     try parser.searchAndParse(path: Fixtures.path(for: "Files.xcassets", sub: .xcassets))
 
     let result = parser.stencilContext()
@@ -33,7 +33,7 @@ class AssetCatalogTests: XCTestCase {
   }
 
   func testImages() throws {
-    let parser = AssetsCatalog.Parser()
+    let parser = try AssetsCatalog.Parser()
     try parser.searchAndParse(path: Fixtures.path(for: "Food.xcassets", sub: .xcassets))
 
     let result = parser.stencilContext()
@@ -41,7 +41,7 @@ class AssetCatalogTests: XCTestCase {
   }
 
   func testAll() throws {
-    let parser = AssetsCatalog.Parser()
+    let parser = try AssetsCatalog.Parser()
     let paths = ["Files.xcassets", "Food.xcassets", "Styles.xcassets"]
     try parser.searchAndParse(paths: paths.map { Fixtures.path(for: $0, sub: .xcassets) })
 
