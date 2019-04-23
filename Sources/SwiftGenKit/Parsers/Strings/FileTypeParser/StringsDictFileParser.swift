e903d9c35f3ff6bd319de219f5c31af3b351f4cf
Florian Fittschen <ffittschen@gmail.com>
2019-04-23 21:33:06 +0200

    Add StringsDictFileParser

--- Sources/SwiftGenKit/Parsers/Strings/FileTypeParser/StringsDictFileParser.swift
+++ Sources/SwiftGenKit/Parsers/Strings/FileTypeParser/StringsDictFileParser.swift
@@ -18,7 +18,18 @@ extension Strings {
         throw ParserError.failureOnLoading(path: path.string)
       }
 
-      return []
+      let plurals = try Strings.StringsDictFileParser.propertyListDecoder.decode([String: StringsDict].self, from: data)
+        .compactMapValues { stringsDict -> StringsDict.PluralEntry? in
+          guard case let .pluralEntry(pluralEntry) = stringsDict else { return nil }
+          return pluralEntry
+        }
+
+      return try plurals.map { keyValuePair -> Entry in
+        let (key, pluralEntry) = keyValuePair
+        let formatValueTypes = pluralEntry.variables.mapValues { "%\($0.valueTypeKey)" }.values
+        let placeholderTypes = try PlaceholderType.placeholders(fromFormat: Array(formatValueTypes).joined(separator: " "))
+        return Entry(key: key, translation: pluralEntry.translation ?? "", types: placeholderTypes)
+      }
     }
   }
 }
---
+++ Sources/SwiftGenKit/Parsers/Strings/StringsDict.swift
@@ -0,0 +1,109 @@
+//
+// SwiftGenKit
+// Copyright © 2019 SwiftGen
+// MIT Licence
+//
+
+import Foundation
+import PathKit
+
+enum StringsDict: Decodable {
+  struct PluralEntry: Codable {
+    struct Variable: Codable {
+      let specTypeKey: String
+      let valueTypeKey: String
+      let zero: String?
+      let one: String?
+      let two: String?
+      let few: String?
+      let many: String?
+      let other: String
+
+      enum CodingKeys: String, CodingKey {
+        case specTypeKey = "NSStringFormatSpecTypeKey"
+        case valueTypeKey = "NSStringFormatValueTypeKey"
+
+        case zero, one, two, few, many, other
+      }
+    }
+
+    let formatKey: String
+    let variables: [String: Variable]
+  }
+
+  struct VariableWidthEntry {
+    let rules: [String: String]
+  }
+
+  case pluralEntry(PluralEntry)
+  case variableWidthEntry(VariableWidthEntry)
+
+  private struct CodingKeys: CodingKey {
+    var intValue: Int?
+    var stringValue: String
+
+    init?(intValue: Int) {
+      self.intValue = intValue
+      self.stringValue = "\(intValue)"
+    }
+    init?(stringValue: String) {
+      self.stringValue = stringValue
+    }
+
+    static let formatKey = CodingKeys.make(key: "NSStringLocalizedFormatKey")
+    static let variableWidthRules = CodingKeys.make(key: "NSStringVariableWidthRuleType")
+    static func make(key: String) -> CodingKeys {
+      return CodingKeys(stringValue: key)!
+    }
+  }
+
+  init(from coder: Decoder) throws {
+    let container = try coder.container(keyedBy: CodingKeys.self)
+    let formatKey = try container.decodeIfPresent(String.self, forKey: .formatKey)
+    let variableWidthRules = try container.decodeIfPresent([String: String].self, forKey: .variableWidthRules)
+
+    switch (formatKey, variableWidthRules) {
+    case (.none, .none), (.some, .some):
+      throw Strings.ParserError.invalidFormat
+    case let (.some(formatKey), .none):
+      var variables = [String: StringsDict.PluralEntry.Variable]()
+      for variableKey in try StringsDict.variableKeysFromFormatKey(formatKey) {
+        variables[variableKey] = try container.decodeIfPresent(StringsDict.PluralEntry.Variable.self, forKey: .make(key: variableKey))
+      }
+      self = .pluralEntry(StringsDict.PluralEntry(formatKey: formatKey, variables: variables))
+    case let (.none, .some(variableWidthRules)):
+      self = .variableWidthEntry(StringsDict.VariableWidthEntry(rules: variableWidthRules))
+    }
+  }
+
+  private static func variableKeysFromFormatKey(_ formatKey: String) throws -> [String] {
+    let pattern = "%#@([\\w\\.\\p{Pd}]+)@"
+    let regex = try NSRegularExpression(pattern: pattern, options: [])
+    let nsrange = NSRange(formatKey.startIndex..<formatKey.endIndex, in: formatKey)
+    let matches = regex.matches(in: formatKey, options: [], range: nsrange)
+
+    return matches.compactMap { match -> String? in
+      let captureGroupRange = match.range(at: 1)
+      guard
+        captureGroupRange.location != NSNotFound,
+        let range = Range(captureGroupRange, in: formatKey)
+      else {
+        return nil
+      }
+
+      return String(formatKey[range])
+    }
+  }
+}
+
+extension StringsDict.PluralEntry {
+  var translation: String? {
+    var result = formatKey
+    variables.forEach { keyValuePair in
+      let (key, variable) = keyValuePair
+      result = result.replacingOccurrences(of: "%#@\(key)@", with: variable.other)
+    }
+
+    return "Plural case 'other': \(result)"
+  }
+}
