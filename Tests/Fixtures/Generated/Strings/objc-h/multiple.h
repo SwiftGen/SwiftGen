b62387294f199347546cf4f354dd0a15c81dbb9a
Eric Slosser <eric.slosser@v-fx.com>
2020-01-22 10:22:01 -0500

    change unit-test expected result to match fixed stencil

--- Tests/Fixtures/Generated/Strings/objc-h/multiple.h
+++ Tests/Fixtures/Generated/Strings/objc-h/multiple.h
@@ -10,17 +10,17 @@ NS_ASSUME_NONNULL_BEGIN
 // alert__title --> "Title of the alert"
 + (NSString*)alertTitle;
 // ObjectOwnership --> "These are %3$@'s %1$d %2$@."
-+ (NSString*)objectOwnershipWithValues:(NSInteger)p1 :(id)p2 :(id)p3
++ (NSString*)objectOwnershipWithValues:(NSInteger)p1 :(id)p2 :(id)p3;
 // percent --> "This is a %% character."
 + (NSString*)percent;
 // private --> "Hello, my name is %@ and I'm %d"
-+ (NSString*)privateWithValues:(id)p1 :(NSInteger)p2
++ (NSString*)privateWithValues:(id)p1 :(NSInteger)p2;
 // types --> "Object: '%@', Character: '%c', Integer: '%d', Float: '%f', CString: '%s', Pointer: '%p'"
-+ (NSString*)typesWithValues:(id)p1 :(char)p2 :(NSInteger)p3 :(float)p4 :(char*)p5 :(void*)p6
++ (NSString*)typesWithValues:(id)p1 :(char)p2 :(NSInteger)p3 :(float)p4 :(char*)p5 :(void*)p6;
 // apples.count --> "You have %d apples"
-+ (NSString*)applesCountWithValue:(NSInteger)p1
++ (NSString*)applesCountWithValue:(NSInteger)p1;
 // bananas.owner --> "Those %d bananas belong to %@."
-+ (NSString*)bananasOwnerWithValues:(NSInteger)p1 :(id)p2
++ (NSString*)bananasOwnerWithValues:(NSInteger)p1 :(id)p2;
 // settings.navigation-bar.self --> "Some Reserved Keyword there"
 + (NSString*)settingsNavigationBarSelf;
 // settings.navigation-bar.title.deeper.than.we.can.handle.no.really.this.is.deep --> "DeepSettings"
