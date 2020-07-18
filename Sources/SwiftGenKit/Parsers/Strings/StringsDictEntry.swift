6743ed55dc9f7bc8e8eb70bb7abb7d4410265af6
Olivier Halligon <olivier@halligon.net>
2020-07-19 01:22:14 +0200

    Replace recursive impl with iterative one

--- Sources/SwiftGenKit/Parsers/Strings/StringsDictEntry.swift
+++ Sources/SwiftGenKit/Parsers/Strings/StringsDictEntry.swift
@@ -132,7 +132,7 @@ extension StringsDict: Decodable {
 // - MARK: Helpers
 
 extension StringsDict {
-  typealias VariableNameResult = (name: String, range: Range<String.Index>, positionalArgument: Int?)
+  typealias VariableNameResult = (name: String, range: NSRange, positionalArgument: Int?)
 
   /// Parses variable names and their ranges from a `NSStringLocalizedFormatKey`.
   /// Every variable name that is contained within the format key is preceded
@@ -148,64 +148,45 @@ extension StringsDict {
     let nsrange = NSRange(formatKey.startIndex..<formatKey.endIndex, in: formatKey)
     let matches = regex.matches(in: formatKey, options: [], range: nsrange)
 
-    return matches.compactMap { match -> VariableNameResult? in
+    return matches.lazy.compactMap { match -> VariableNameResult? in
       // the whole format string including delimeters
       let fullMatchNSRange = match.range(at: 0)
       // 1st capture group is the positional argument of the format string (Optional!)
       let positionalArgumentNSRange = match.range(at: 1)
+      let positionalArgumentRange = Range(positionalArgumentNSRange, in: formatKey)
       // 2nd capture group is the name of the variable, the string in between the delimeters
       let nameNSRange = match.range(at: 2)
-
-      guard let fullMatchRange = Range(fullMatchNSRange, in: formatKey) else { return nil }
       guard let nameRange = Range(nameNSRange, in: formatKey) else { return nil }
-      guard let positionalArgumentRange = Range(positionalArgumentNSRange, in: formatKey) else {
-        return (String(formatKey[nameRange]), fullMatchRange, nil)
-      }
 
-      return (String(formatKey[nameRange]), fullMatchRange, Int(formatKey[positionalArgumentRange]))
+      let position = positionalArgumentRange.flatMap { Int(formatKey[$0]) }
+      return (String(formatKey[nameRange]), fullMatchNSRange, position)
     }
   }
 }
 
 extension StringsDict.PluralEntry {
-  // Extract the placeholders (`NSStringFormatValueTypeKey`) from the different variable definitions
-  // into a single flattened list of placeholders
+  /// Extract the placeholders (`NSStringFormatValueTypeKey`) from the different variable
+  /// definitions into a single flattened list of placeholders
   var formatKeyWithVariableValueTypes: String {
     var result = formatKey
+    var offset = 0
 
-    for intermediateResult in sequence(first: result, next: replaceFirstVariableInFormatKey(_:)) {
-      result = intermediateResult
+    for (name, var nsrange, positionalArgument) in StringsDict.variableNames(fromFormatKey: formatKey) {
+      guard let variable = variables.first(where: { $0.name == name }) else { continue }
+
+      let variablePlaceholder: String
+      if let positionalArgument = positionalArgument {
+        variablePlaceholder = "%\(positionalArgument)$\(variable.rule.valueTypeKey)"
+      } else {
+        variablePlaceholder = "%\(variable.rule.valueTypeKey)"
+      }
+
+      nsrange.location += offset
+      guard let range = Range(nsrange, in: result) else { continue }
+      result.replaceSubrange(range, with: variablePlaceholder)
+      offset += variablePlaceholder.count - nsrange.length
     }
 
     return result
-  }
-
-  /// Replaces the first encountered variable in a `NSStringLocalizedFormatKey` with its `NSStringFormatValueTypeKey`.
-  ///
-  /// This method should be used in a recursive context, in which the output will be used as the input for the
-  /// next iteration.
-  ///
-  /// - Parameter formatKey: A format key containing variables.
-  /// - Returns: The format key in which the first encountered variable is replaced
-  /// with its `NSStringFormatValueTypeKey` if the `formatKey` contained any variables, `nil` otherwise.
-  private func replaceFirstVariableInFormatKey(_ formatKey: String) -> String? {
-    guard let variableNameResult = StringsDict.variableNames(fromFormatKey: formatKey).first else {
-      return nil
-    }
-
-    let (name, range, positionalArgument) = variableNameResult
-    guard let variable = variables.first(where: { $0.name == name }) else { return nil }
-    guard range.lowerBound >= formatKey.indices.startIndex, range.upperBound <= formatKey.indices.endIndex else {
-      return nil
-    }
-
-    let variablePlaceholder: String
-    if let positionalArgument = positionalArgument {
-      variablePlaceholder = "%\(positionalArgument)$\(variable.rule.valueTypeKey)"
-    } else {
-      variablePlaceholder = "%\(variable.rule.valueTypeKey)"
-    }
-
-    return formatKey.replacingCharacters(in: range, with: variablePlaceholder)
   }
 }
