e507e85fe7b5b5f98914045c09a42dc0f4705999
David Jennes <david.jennes@gmail.com>
2020-10-07 03:29:54 +0200

    Generate output

--- Tests/Fixtures/Generated/Colors/literals-swift5/defaults.swift
+++ Tests/Fixtures/Generated/Colors/literals-swift5/defaults.swift
@@ -1,7 +1,7 @@
 // swiftlint:disable all
 // Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
-#if os(OSX)
+#if os(macOS)
   import AppKit
   internal enum ColorName { }
 #elseif os(iOS) || os(tvOS) || os(watchOS)
