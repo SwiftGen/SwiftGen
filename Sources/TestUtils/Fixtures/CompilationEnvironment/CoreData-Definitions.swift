dea3d0a305bcba71ce35aa24fa2292fb38ab17f0
David Jennes <david.jennes@gmail.com>
2022-07-23 02:13:40 +0200

    Add test case

--- Sources/TestUtils/Fixtures/CompilationEnvironment/CoreData-Definitions.swift
+++ Sources/TestUtils/Fixtures/CompilationEnvironment/CoreData-Definitions.swift
@@ -6,3 +6,14 @@ public enum IntegerEnum: Int16 {
 public enum StringEnum: String {
   case test1
 }
+public struct IntegerOptionSet: OptionSet {
+  public let rawValue: Int16
+  public init(rawValue: Int16) {
+    self.rawValue = rawValue
+  }
+
+  public static let foo = IntegerOptionSet(rawValue: 1 << 0)
+  public static let bar = IntegerOptionSet(rawValue: 1 << 1)
+
+  public static let all: IntegerOptionSet = [.bar, foo]
+}
