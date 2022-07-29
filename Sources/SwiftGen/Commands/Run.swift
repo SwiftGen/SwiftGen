da4d1b11a936a3318fa3a6cfb0430383fd83af58
David Jennes <david.jennes@gmail.com>
2022-07-30 00:08:14 +0200

    Add mechanism to switch to new (smart) space trimming

--- Sources/SwiftGen/Commands/Run.swift
+++ Sources/SwiftGen/Commands/Run.swift
@@ -6,6 +6,7 @@
 
 import ArgumentParser
 import PathKit
+import Stencil
 import StencilSwiftKit
 import SwiftGenCLI
 import SwiftGenKit
@@ -57,6 +58,9 @@ extension Commands.Run {
     @OptionGroup
     var output: Commands.OutputDestination
 
+    @OptionGroup(visibility: .hidden)
+    var spacing: Commands.ExperimentalSpacing
+
     @Argument(help: .init(Parser.info.pathDescription, valueName: "path"))
     var paths: [Path]
 
@@ -70,11 +74,7 @@ extension Commands.Run {
       try parser.searchAndParse(paths: paths, filter: filter)
 
       let templateRealPath = try template.reference.resolvePath(forParser: Parser.info)
-      let template = try StencilSwiftTemplate(
-        templateString: templateRealPath.read(),
-        environment: stencilSwiftEnvironment()
-      )
-
+      let template = try Template.load(from: templateRealPath, modernSpacing: spacing.modernSpacing)
       let context = parser.stencilContext()
       let enriched = try StencilContext.enrich(context: context, parameters: templateParameters)
       let rendered = try template.render(enriched)
