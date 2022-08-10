5589af0379344088fce227565d77cf5b1b65e6f1
David Jennes <david.jennes@gmail.com>
2022-08-10 23:54:00 +0200

    Ensure we use the right version of  arg. parser

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     .library(name: "SwiftGenKit", targets: ["SwiftGenKit"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.3"),
     .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.1"),
     .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
     .package(url: "https://github.com/krzysztofzablocki/Difference.git", branch: "master"),
-    .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.15.0"),
+    .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.15.1"),
     .package(url: "https://github.com/shibapm/Komondor.git", exact: "1.1.3"),
-    .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.10.0"),
+    .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", from: "2.10.1"),
     .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.7")
   ],
   targets: [
